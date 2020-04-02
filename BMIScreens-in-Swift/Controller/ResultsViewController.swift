5914c2f9dd9b246be0313086eb94522dab61dece
Tommybarral <isamutom27@hotmail.com>
2020-04-02 12:46:47 +0200

    First update of BMI calculation

---
+++ BMIScreens-in-Swift/Controller/ResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewController.swift
+//  BMIScreens-in-Swift
+//
+//  Created by tommy on 02/04/2020.
+//  Copyright © 2020 tommy. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    var bmiValue: String?
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultLabel.text = bmiValue
+    }
+
+    @IBAction func recalc(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ BMIScreens-in-Swift/Controller/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  BMIScreens-in-Swift
+//
+//  Created by tommy on 02/04/2020.
+//  Copyright © 2020 tommy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var bmiValue = "0.0"
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightCalc: UISlider!
+    @IBOutlet weak var weightCalc: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func heightSlider(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calc(_ sender: UIButton) {
+        let height = heightCalc.value
+        let weight = weightCalc.value
+        let bmi = weight / pow(height, 2)
+        bmiValue = String(format: "%.1f", bmi)
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let resultView = segue.destination as! ResultsViewController
+            resultView.bmiValue = bmiValue
+        }
+    }
+}
+
